e1e7286ada9695fe9d275542f104f96e56641d47
alex luu <alexanderaluu@gmail.com>
2024-03-25 14:20:47 -0400

    hello

--- pages/homePage.spec.ts
+++ pages/homePage.spec.ts
@@ -7,12 +7,14 @@ export class HomePage {
     //vars
     readonly page: Page;
     readonly accountId: Locator;
+    readonly newAccountId: Locator;
 
     //constructor
     constructor(page: Page) {
         //locators
         this.page = page;
         this.accountId = page.locator("div[id='top-header-menu'] b");
+        this.newAccountId = page.locator(".selectable");
     }
 
     //methods
@@ -29,5 +31,10 @@ export class HomePage {
         const accId = await this.accountId.textContent();
         this.setValue("accountId", accId!);
         // console.log('this is the account id: ' +this.dataMap.get("accountId"));
+    }
+
+    async getNewAccountId(){
+        const name = await this.newAccountId.innerText();
+        this.setValue("name", name!);
     }
 }
